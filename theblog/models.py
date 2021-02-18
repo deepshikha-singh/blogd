e7e15c29fda837ab859c8897797d3d761531bd1e
deepshikha-singh <deepshikha20k@gmail.com>
2021-02-18 17:58:07 +0530

    work on blogd

---
+++ theblog/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+from datetime import datetime, date
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=225)
+    title_tag = models.CharField(max_length=225)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    post_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title + ' | ' + str(self.author)
+    
+    def get_absolute_url(self):
+        #return reverse("article-detail", args=(str
+        return reverse('home')
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    body = models.TextField(null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '%s - %s' % (self.post.title, self.name)
+    
+       
