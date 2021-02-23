3184c9aaf6061e6931105dce2daf5cc8d2011ed7
deepshikha-singh <deepshikha20k@gmail.com>
2021-02-23 18:36:12 +0530

    work on blogd

--- theblog/urls.py
+++ theblog/urls.py
@@ -1,15 +1,18 @@
 from django.urls import path
 #from django.conf.urls import url
 #from .import views
-from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, AddCommentView
+from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, AddCommentView, AddCategoryView, CategoryView, CategoryListView
 
 urlpatterns = [
     #path('', views.home, name="home"),
     path('', HomeView.as_view(), name="home"),
     path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
     path('add_post/', AddPostView.as_view(), name='add_post'),
+    path('add_category/', AddCategoryView.as_view(), name='add_category'),
     path('article/edit/<int:pk>', UpdatePostView.as_view(), name='update_post'),
     path('article/<int:pk>/remove', DeletePostView.as_view(), name='delete_post'), 
     path('article/<int:pk>/comment/', AddCommentView.as_view(), name='add_comment'),
+    path('category/<str:cats>/', CategoryView, name="category"),
+    path('category-list/', CategoryListView, name="category-list"),
 
 ]  
